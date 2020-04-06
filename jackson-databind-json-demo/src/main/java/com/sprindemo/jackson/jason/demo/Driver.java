14580d15ef4c0c996c4531ca415dbede1dbad883
Hardik Khanna <khannahardik007@gmail.com>
2020-04-06 12:20:22 +0530

    Jackson Data Binding

---
+++ jackson-databind-json-demo/src/main/java/com/sprindemo/jackson/jason/demo/Driver.java
@@ -0,0 +1,36 @@
+package com.sprindemo.jackson.jason.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String args[]) {
+
+		try {
+			// create Object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// read jason file and map to java pojo
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			// print first name and last name
+			System.out.println("First Name  = " + theStudent.getFirstName());
+			System.out.println("Last Name = " + theStudent.getLastName());
+
+			Address tempAdd = theStudent.getAddress();
+
+			System.out.println("Street = " + tempAdd.getStreet());
+			System.out.println("City = " + tempAdd.getCity());
+
+			for (String tempLang : theStudent.getLanguages()) {
+				System.out.println(tempLang);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
