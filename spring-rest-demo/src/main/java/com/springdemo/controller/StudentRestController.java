0cee2dfbd64144bdc118b5a81047beadf5fa3a38
Hardik Khanna <Khannahardik007@gmail.com>
2020-06-15 23:28:02 +0530

    Added Global Exception Handling to Rest Service

--- spring-rest-demo/src/main/java/com/springdemo/controller/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/springdemo/controller/StudentRestController.java
@@ -3,7 +3,10 @@ package com.springdemo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,32 @@ import com.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
+	private List<Student> theStudents;
+
+	// define @PostContruct to load the Student Data
+
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<Student>();
+		theStudents.add(new Student("Hardik", "Khanna"));
+		theStudents.add(new Student("Oshin", "Dhaliwal"));
+		theStudents.add(new Student("Oshin", "Dhaliwal"));
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		if (studentId >= theStudents.size() || studentId < 0) {
+			throw new StudentNotFoundException("Student Id not Found - " + studentId);
+		}
+
+		return theStudents.get(studentId);
+
+	}
+
 	// define endpoint for "/student"
 	@GetMapping("/students")
 	public List<Student> getStudent() {
-
-		List<Student> theStudents = new ArrayList<Student>();
-		theStudents.add(new Student("Hardik", "Khanna"));
-		theStudents.add(new Student("Oshin", "Dhaliwal"));
 
 		return theStudents;
 	}
