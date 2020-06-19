58fb2d0d02498a80082e5d736a1b3e4428c416f9
Hardik khanna <khannahardik007@gmail.com>
2020-06-19 17:49:26 +0530

    Get request for CRM APP with Exception Handling

---
+++ spring-crm-rest-demo/src/main/java/com/springdemo/rest/controller/CustomerRestController.java
@@ -0,0 +1,39 @@
+package com.springdemo.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springdemo.rest.entity.Customer;
+import com.springdemo.rest.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+
+		return customerService.getCustomers();
+	}
+
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+
+		Customer theCustomer = customerService.getCustomer(customerId);
+
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+
+		return theCustomer;
+	}
+
+}
